ab6e40a33c83e36ccc914f54a381a510ec330afb
Felipe Mandiola <hi@icarusmind.com>
2026-04-24 15:50:17 -0400

    dsajasdmasg

--- src/app/rabbit-hole/_components/mdx-components.tsx
+++ src/app/rabbit-hole/_components/mdx-components.tsx
@@ -45,7 +45,7 @@ export function Co({ name, label }: { name: string; label?: string }) {
 
 export function Callout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="-mx-5 border border-[#FFEC40] bg-[#FFEC40]/10 p-5 my-6">
+    <div className="md:-mx-5 border border-[#FFEC40] bg-[#FFEC40]/10 p-5 my-6">
       {children}
     </div>
   );
